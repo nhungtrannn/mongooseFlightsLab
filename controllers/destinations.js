f2240d14985fb72f27cb5dd7b383cd845dfc760f
Nhung Tran <nhungtrannn@gmail.com>
2019-02-19 14:35:07 -0600

    lab1 and lab2 completlete

---
+++ controllers/destinations.js
@@ -0,0 +1,20 @@
+var Flight = require('../models/flight');
+ 
+ module.exports = {
+   create
+ };
+
+ function create (req,res){
+    Flight.findById(req.params.id, function(err, flight){
+        console.log(req.body);
+        flight.destinations.push(req.body);
+        flight.save(function(err) {
+            if(err){
+                console.log(err)
+            }else{
+                res.redirect(`/flights/${flight._id}`)
+            }
+ 
+        })
+    });
+ }
